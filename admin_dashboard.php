147c88698561524f96dc12dd1a7db4c1f91ac897
abdumehammed <39337892+abdumehammed@users.noreply.github.com>
2025-02-05 02:17:32 -0500

    vice change

--- admin_dashboard.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['username']) || $_SESSION['role'] != 'admin') {
-    header("Location: login.html");
-    exit;
-}
-echo "<h1>Welcome, Admin!</h1>";
-?>
